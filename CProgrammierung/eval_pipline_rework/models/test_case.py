251f272a7b7dc7d4be87607e54e4e19db00770ed
Mark Spitzner <m.spitzner001@googlemail.com>
2020-05-15 12:54:46 +0200

    added doc-string

--- CProgrammierung/eval_pipline_rework/models/test_case.py
+++ CProgrammierung/eval_pipline_rework/models/test_case.py
@@ -1,10 +1,21 @@
+"""Implements abstraction for TestCase type"""
+
+
 class TestCase:
+    """Abstraction of TestCase Type consists of path input, output, error"""
     path: str = ""
     test_input: str = ""
     test_output: str = ""
     error_expected: bool = False
 
     def __init__(self, path, test_input, test_output, error_expected):
+        """
+        :param path: base path where this testcase is loaded from
+        :param test_input: test input
+        :param test_output:
+        expected output for error testcases this is an empty string
+        :param error_expected: indicates whether an error is expected or not.
+        """
         self.path = path
         self.test_input = test_input
         self.test_output = test_output
@@ -15,3 +26,20 @@ class TestCase:
                f"Input:\n{self.test_input}\n" \
                f"Output:\n{self.test_output}\n" \
                f"Error expected: {self.error_expected}\n"
+
+    def __eq__(self, other):
+        """
+        defines quality,
+        which is the case if they are loaded from the same source
+        """
+        if isinstance(self.__class__, other):
+            return self.path == other.path
+
+        return False
+
+    def __ne__(self, other):
+        """
+        defines inequality,
+        which is the case if they are not equal
+        """
+        return not self.__eq__(other)

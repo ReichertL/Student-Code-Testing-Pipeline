7aec7eb1a30d8e1460b6d63fe919696c7f6b86c1
Leonie Reichert <reicherl@hu-berlin.de>
2021-06-03 18:59:25 +0200

    added rlimit to database schema and doc

--- CProgrammierung/eval_pipline/logic/test_case_executor.py
+++ CProgrammierung/eval_pipline/logic/test_case_executor.py
@@ -369,10 +369,11 @@ class TestCaseExecutor:
         return result, valgrind_output
     
     def get_limits(self,testcase):
-        if not self.args.final:# and testcase.rlimit==None:
+        if not self.args.final and testcase.rlimit==None:
             return [self.configuration["RLIMIT_DATA"],self.configuration["RLIMIT_STACK"],self.configuration["RLIMIT_CPU"]]
-        #elif not self.args.final:
-         #   return [testcase.rlimit,self.configuration["RLIMIT_STACK"],self.configuration["RLIMIT_CPU"]]
+        elif not self.args.final:
+            data_limit=max(testcase.rlimit,10000000)
+            return [data_limit,self.configuration["RLIMIT_STACK"],self.configuration["RLIMIT_CPU"]]
         else: 
             return [self.configuration["RLIMIT_DATA_CARELESS"],self.configuration["RLIMIT_STACK_CARELESS"],self.configuration["RLIMIT_CPU_CARELESS"]]  
             
@@ -385,8 +386,7 @@ class TestCaseExecutor:
         nothing
         """
         global limits
-        data_limit=max(limits[0],10000000)
-        resource.setrlimit(resource.RLIMIT_DATA,2 * (data_limit,))
+        resource.setrlimit(resource.RLIMIT_DATA,2 * (limits[0],))
         resource.setrlimit(resource.RLIMIT_STACK,2 * (limits[1],))
         resource.setrlimit(resource.RLIMIT_CPU,2 * (limits[2],))
 

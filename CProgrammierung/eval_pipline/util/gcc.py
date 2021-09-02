035032e50409382b3700c59bf0af778a38676f2a
Leonie Reichert <reicherl@hu-berlin.de>
2021-09-02 10:07:30 +0200

    working on final funktion

--- CProgrammierung/eval_pipline/util/gcc.py
+++ CProgrammierung/eval_pipline/util/gcc.py
@@ -118,14 +118,17 @@ def docker_gcc(gcc_args, src, dest, docker_image, docker_container, directory):
     all_args = gcc_args + ['-o', os.path.basename(dest),
                            os.path.basename(dest) + '.c']
     commandline = ' '.join(all_args)
-    cp=run(SUDO_DOCKER +
+    
+    command_full= SUDO_DOCKER +
         ['exec',
          '-w', '/host',
          docker_container,
          'bash', '-c',
          f'{commandline} 2> gcc.stderr ; '
          'echo $? > gcc.return ;'
-         f'chown {OWN_UID_GID} gcc.stderr gcc.return {os.path.basename(dest)}'],
+         f'chown {OWN_UID_GID} gcc.stderr gcc.return {os.path.basename(dest)}']
+    logging.debug(command_full)
+    cp=run(command_full,
         stdout=DEVNULL,
         stderr=DEVNULL)
     # collect gcc's stderr

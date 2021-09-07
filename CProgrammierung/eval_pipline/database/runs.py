aefa91db97b3c2f6e99e2df001cbbb9a420c888c
Leonie Reichert <reicherl@hu-berlin.de>
2021-09-07 11:01:37 +0200

    debugging performance function

--- CProgrammierung/eval_pipline/database/runs.py
+++ CProgrammierung/eval_pipline/database/runs.py
@@ -94,7 +94,9 @@ class Run(Base):
         logging.debug(name)
         results=dbm.session.query(Run, sub.Submission)\
             .join(sub.Submission).join(stud.Student)\
-            .filter(Run.passed==True, Run.manual_overwrite_passed!=True, sub.Submission.is_fast==True, stud.Student.name==name).all()
+            .filter(Run.passed==True)\
+            .filter(Run.manual_overwrite_passed.is_(False))\#Run.manual_overwrite_passed!=True)\
+            .filter(sub.Submission.is_fast==True, stud.Student.name==name).all()
         performance=list()
         logging.debug(results)
         for run,submission in results:

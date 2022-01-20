983d2a61dcffe9b30ffd05d80c031e66e6750ef8
Leonie Reichert <leonie.reichert@informatik.hu-berlin.de>
2022-01-20 20:35:33 +0100

    renamed gruenau in all files as reference system

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # build images
 rm -fr /tmp/dockershared 2> /dev/null
 mkdir /tmp/dockershared
-sudo docker build -t gruenau_clone gruenau/.
+sudo docker build -t reference-clone reference-server-clone/.
 sudo docker build -t pipeline pipeline/.
 
 #create a shared space where the containers can exchange files
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #start containers in detached mode, with the same socket so they can see each other. /tmp/dockershared is used as a shread space for file exchanges
-sudo docker create -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/dockershared:/host --name gruenau_clone gruenau_clone
+sudo docker create -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/dockershared:/host --name reference-clone reference-clone
 #sudo docker run -td --name gruenau_clone -v /var/run/docker.sock:/var/run/docker.sock gruenau_clone
 sudo docker run -t -d --name pipeline -v /var/run/docker.sock:/var/run/docker.sock  -v /tmp/dockershared:/tmp/dockershared -v /home/reichert/Software_and_Development/dockerfile_for_evalpipline/pipeline/eval_pipeline:/home/ubuntu/eval_pipeline --network="host" pipeline 
 
